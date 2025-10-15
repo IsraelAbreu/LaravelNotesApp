0a9f600bf12a549ca18064ff2a4ad06d52ffda61
IsraelAbreu <israeloliveira18@hottmail.com>
2025-10-15 20:55:54 -0300

    Validação: Definindo mensagem personalizadas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,9 +15,18 @@ class AuthController extends Controller
     {
         //form validation
         $request->validate(
+            //rules
             [
-                'text_username' => 'required|min:6',
-                'text_password' => 'required'
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            //messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username deve ser um e-mail válido',
+                'text_password.required' => 'A senha é obrigatória',
+                'text_password.min' => 'A senha precisa ter no mínimo :min caracteres',
+                'text_password.max' => 'A senha só pode ter no máximo :max caracteres',
             ]
         );
 
