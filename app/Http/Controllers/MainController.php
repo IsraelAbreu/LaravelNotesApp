d58d8f23711bc5ee02821806a1cfa22f84d16028
IsraelAbreu <israeloliveira18@hottmail.com>
2025-10-19 10:36:27 -0300

    View: Exibindo as notas do usuário na home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,14 +11,9 @@ class MainController extends Controller
     {
        //load user's note
         $id = session('user');
-        $user = User::find($id)->toArray();
         $notes = User::find($id)->notes()->get()->toArray();
-        echo '<pre>';
-        print_r($user);
-        print_r($notes);
-        die();
        //show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
